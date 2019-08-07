12011dcfc4b6186d2ede7469c035d153e7cf9b53
Brian Neisler <me@brianneisler.com>
2019-08-07 16:20:18 -0700

    Get build working

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import '../semantic/dist/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <App />,
